b53a1ed6e1c0852c6093b2fe2f05680526163815
Guest User <Guest@iMacs-iMac4.local>
2026-09-04 12:29:58 +0800

    not finished

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -7,7 +7,17 @@ export default function CategoryFilter({ activeFilter, onFilterChange }) {
           - Each button calls onFilterChange(f) on click.
           - The active button should look different from the others
             (e.g., "btn-primary" for active, "btn-outline" otherwise).
-          - Don't forget the `key` prop! */}
+          - Don't forget the `key` prop! */
+        ["all", "watched", "unwatched"].map((filter) => (
+        <button
+        key={filter}
+        className={`btn join-item ${currentFilter === filter ? "btn-primary" : "btn-ghost"}`}
+        onClick={() => onChangeFilter(filter)}
+        type="button"
+      >
+        {filter[0].toUpperCase() + filter.slice(1)}
+      </button>
+    ))}
     </div>
   );
 }
