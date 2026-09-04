d63f53ba0d7554667254e0e62b944df98e81a904
fraciscoejay_max <fraciscoejay@gmail.com>
2026-09-05 06:47:13 +0800

    finished categoryfilter and recipe card

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -8,16 +8,19 @@ export default function CategoryFilter({ activeFilter, onFilterChange }) {
           - The active button should look different from the others
             (e.g., "btn-primary" for active, "btn-outline" otherwise).
           - Don't forget the `key` prop! */
-        ["all", "watched", "unwatched"].map((filter) => (
-        <button
-        key={filter}
-        className={`btn join-item ${currentFilter === filter ? "btn-primary" : "btn-ghost"}`}
-        onClick={() => onChangeFilter(filter)}
-        type="button"
-      >
-        {filter[0].toUpperCase() + filter.slice(1)}
-      </button>
-    ))}
+        FILTERS.map((filter) => ( 
+          <button
+           key={filter} 
+           className={`btn ${ 
+            activeFilter === filter ? "btn-primary" : "btn-outline"
+          }`} 
+          onClick={() => onFilterChange(filter)} 
+          type="button" 
+        > 
+          {filter} 
+        </button> 
+        ))
+      }
     </div>
   );
 }
